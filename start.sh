11e55d059493e0c6db22eb36329c5aefc97cb321
fastleopard9372 <fastleopard9372@gmail.com>
2025-07-01 12:41:44 +0000

    fixed

--- start.sh
+++ start.sh
@@ -24,3 +24,15 @@ sleep 3
 # Start the application
 echo "Starting FastAPI application..."
 exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8000}
+
+#!/usr/bin/env bash# 
+# pip install -r requirements.txt
+# prisma generate 
+# # Store/pull Prisma cache with build cache
+# if [[! -d $PRISMA_BINARY_CACHE_DIR]]; 
+# then echo "...Copying Prisma Binary Cache from Build Cache" 
+#     cp -R $XDG_CACHE_HOME/prisma/binaries $PRISMA_BINARY_CACHE_DIR
+# else 
+#     echo "...Storing Prisma Binary Cache in Build Cache" 
+#     cp -R $PRISMA_BINARY_CACHE_DIR $XDG_CACHE_HOME
+# fi
