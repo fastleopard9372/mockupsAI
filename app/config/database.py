b292e5f9b3fcf129feb77c3ab40d55ea7233c7ec
fastleopard9372 <fastleopard9372@gmail.com>
2025-07-01 13:53:29 +0000

    fixed

--- app/config/database.py
+++ app/config/database.py
@@ -2,7 +2,7 @@ from prisma import Prisma
 from prisma.errors import PrismaError
 from app.config.settings import settings
 import logging
-
+import os
 logger = logging.getLogger(__name__)
 
 # Global database instance
